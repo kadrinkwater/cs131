67ba1c3472dfa406f684774e285303b57d87d464
Kell Drinkwater <mizunomi@gmail.com>
2026-07-09 13:36:54 -0700

    more sed/awk

--- 0629/exercise.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Exercise: Given two directory names,
-#Compare the number of regular files in each dir, and
-#List all filenames in whichever one has more, in sorted order
-
-dir1 = $1
