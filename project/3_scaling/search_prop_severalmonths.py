87aa4adfcef6c15a05f0f8e23eb55d83b03b2e68
Kell Drinkwater <mizunomi@gmail.com>
2026-08-02 20:58:54 -0700

    Preliminary results checking % referrals from search, checking 1 month in each year in data

--- project/3_scaling/search_prop_severalmonths.py
+++ project/3_scaling/search_prop_severalmonths.py
@@ -1,9 +1,10 @@
 # Analyze proportion of clicks coming from other-search
 # for several (hardcoded) months of clickstream data.
 
-import sys, time, os
+import sys, time
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as fns
+from google.cloud import storage
 
 
 def sumClicks(frame):
@@ -17,12 +18,6 @@ def sumClicks(frame):
 
 
 def analyzeMonth(path):
-  #Check if path exists and is a file
-  if not os.path.isfile(path):
-    print(f"File does not exist (or is not a file): {path}")
-    return
-
-
   #Extract the year-month from the filename
   #which are like clickstream-enwiki-2017-11.tsv
   #path = sys.argv[1]
@@ -36,7 +31,7 @@ def analyzeMonth(path):
   df = spark_sesh.read.schema(schema).options(header=False, delimiter="\t").csv(path, inferSchema=False)
   #Check loaded correctly
   print(f"==== {yearmonth} ====")
-  print("Total rows: ", df.count())
+  print(f"Total rows: {df.count():,}")
 
   #Calculate number of clicks in total / from search
   totalClicks = sumClicks(df)
@@ -49,12 +44,17 @@ def analyzeMonth(path):
   print(f"{proportion:.2f}% from search")
 
 
+#Set up storage client / bucket access (to check whether files exist)
+storageClient = storage.Client()
+bucketName = 'phonic-envoy-499516-i9-cs131'
+bucket = storageClient.bucket(bucketName)
+
 
 # Make list of file paths to analyze.
 # For this run, we are doing all Octobers in the dataset.
 # Paths are like [BUCKET]/decompressed/clickstream-enwiki-2017-11.tsv
-years = [17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-paths = [f'../decompressed/clickstream-enwiki-20{yy}-10.tsv' for yy in years]
+years = [18, 19, 20, 21, 22, 23, 24, 25]
+#paths = [f'../decompressed/clickstream-enwiki-20{yy}-10.tsv' for yy in years]
 
 
 #Create SparkSession
@@ -65,8 +65,26 @@ startTime = time.time()
 
 #Call analysis function
 print("========= BEGIN ANALYSIS ==========")
-for p in paths:
-  analyzeMonth(p)
+for yy in years:
+  partialPath = f'decompressed/clickstream-enwiki-20{yy}-10.tsv'
+  fullPath = f'gs://{bucketName}/{partialPath}'
+  #Check if path exists and is a file
+  # if not os.path.isfile(path):
+  #   print(f"File does not exist (or is not a file): {path}")
+  #   return
+  # blobExists = storage.Blob(bucket=bucket, name=partialPath)
+  # if not blobExists:
+  #   print(f"Error: Nonexistent Path {partialPath}")
+  # else:
+  #   analyzeMonth(fullPath)
+  # try:
+  #   analyzeMonth(fullPath)
+  # except AnalysisError as e:
+  #   print(f'caught {type(e)} with nested {e.exceptions}')
+  analyzeMonth(fullPath)
+  #I HAVE TRIED SEVERAL WAYS TO CHECK FOR FILE NONEXISTENCE
+  #I GIVE UP FOR NOW WE WILL JUST WORK OFF A MANUALLY CORRECTED LIST
+  #OF FILES THAT DO EXIST
 
 #Stop timer
 computeTime = time.time() - startTime
